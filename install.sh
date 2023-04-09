5c8d918209d503e5c9c2b4ff588c6fbd1c507a1f
อุ้ อุ้ อ๊า อ๊า อุก่ะ อุก่ะ <120770468+rushmi0@users.noreply.github.com>
2023-04-09 14:45:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 sudo apt-get install python3-pyqt5 libsecp256k1-0 python3-cryptography python3-setuptools python3-pip pip -y
 
+
 pip install pyfiglet
 
 wget https://download.electrum.org/4.3.4/Electrum-4.3.4.tar.gz
@@ -11,6 +12,8 @@ python3 -m pip install --user Electrum-4.3.4.tar.gz
 electrum setconfig rpcport 7777
 electrum setconfig rpcuser VM
 electrum setconfig rpcpassword VM123
+
+sudo apt-get update -y
 
 echo 'นึกแล้วมึงต้องอ่าน!!'
 reboot
