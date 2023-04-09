0ae102ad555b36aeda7f4b0f5c856c9e330c7dfa
อุ้ อุ้ อ๊า อ๊า อุก่ะ อุก่ะ <120770468+rushmi0@users.noreply.github.com>
2023-04-09 15:06:23 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 sudo apt-get install python3-pyqt5 libsecp256k1-0 python3-cryptography python3-setuptools python3-pip pip -y
 
-
 pip install pyfiglet
 
 wget https://download.electrum.org/4.3.4/Electrum-4.3.4.tar.gz
